a4edd5808e15e853d900435f1e0923bb1cd4f1fa
Tom Ecrepont <tomecrepont@gmail.com>
2022-10-11 14:09:59 +0200

    submit email

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,40 @@
 let  contactSubmitButtons = document.querySelectorAll('button[type=submit]')
-let sendEmailToServer = (event) => {
-    console.log('HOE')
-    event.preventDefault()
+let formSubmittedSuccessTemplate = document.getElementById('form-submitted-success')
+let formSubmittedErrorTemplate = document.getElementById('form-submitted-error')
+
+let useTemplate = (nodeToReplace, template) => {
+    let content = template.content.cloneNode(true) 
+    let parentNode = nodeToReplace.parentNode
+    parentNode.replaceChild(content, nodeToReplace)
+} 
+
+let displaySuccessMessage = (nodeToReplace) => {
+    useTemplate(nodeToReplace, formSubmittedSuccessTemplate)
 }
-console.log(contactSubmitButtons)
+let displayErrorMessage = (nodeToReplace) => {
+    useTemplate(nodeToReplace, formSubmittedErrorTemplate)
+}
+
+let sendEmailToServer = (formData, nodeToReplace) => {
+    axios.post('http://127.0.0.1:3000/sales/leads.json', formData)
+      .then(function () {
+        displaySuccessMessage(nodeToReplace);
+      })
+      .catch(function () {
+        displayErrorMessage(nodeToReplace);
+      });
+
+}
+
+let handleFormSubmission = (event) => {
+    event.preventDefault()
+    const formData = new FormData(event.target)
+    sendEmailToServer(
+        formData,
+        event.target
+    )
+}
+
 contactSubmitButtons.forEach(button => {
-    button.onsubmit = event => sendEmailToServer(event)
+    button.form.onsubmit = event => handleFormSubmission(event)
 });
-console.log(contactSubmitButtons[0].onsubmit)
